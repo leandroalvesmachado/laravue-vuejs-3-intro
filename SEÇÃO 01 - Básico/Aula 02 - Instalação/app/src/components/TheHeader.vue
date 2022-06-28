da676c84bc822844ce4ddabd33c3a7d95ca72052
Leandro Machado <leandroalvesmachado@gmail.com>
2022-06-28 13:16:16 -0300

    fim da aula 03 SFC

---
+++ SEÇÃO 01 - Básico/Aula 02 - Instalação/app/src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="header">
+    Header
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+
+  }
+}
+</script>
+
+<style>
+  .header {
+    background: #000;
+    color: #fff;
+  }
+</style>
