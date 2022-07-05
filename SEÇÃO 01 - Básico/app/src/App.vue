06c1efab6ad1b847d12a9a39b5e1c44a068889c8
Leandro Machado <leandroalvesmachado@gmail.com>
2022-07-05 15:46:28 -0300

    salvando

---
+++ SEÇÃO 01 - Básico/app/src/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <TheHeader v-if="showHeader" />
+
+    <div v-show="showName">
+        Nome: {{ firstName }} <br>
+        Sobrenome: {{ lastName }}
+    </div>
+
+    <div v-if="accessLevel === 'admin'">Usuário Admin</div>
+    <div v-else-if="accessLevel === 'marketing'">Usuário Marketing</div>
+    <div v-else>Usuário normal</div>
+
+    <div
+      v-for="obj in todos"
+      v-bind:key="obj.id"
+    >
+      {{ obj.title }}
+    </div>
+
+    <br>
+    
+    <div
+      v-for="(obj, index) in todos"
+      v-bind:key="obj.id"
+    >
+      {{ index }} - {{ obj.title }}
+    </div>
+
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      firstName: 'Jon',
+      lastName: 'Snow',
+      showName: false,
+      accessLevel: 'admin',
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
